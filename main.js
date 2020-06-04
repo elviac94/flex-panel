e00f6e47855f9024518018b3fa91dfce4d7c2e82
elviac94 <elvi_ac94@hotmail.com>
2020-06-04 14:32:17 +0200

    add js

---
+++ main.js
@@ -0,0 +1,7 @@
+'use-strict';
+const panels= document.querySelectorAll('.panel');
+function toggleOpen(){
+    this.classList.toggle('open');
+}
+
+panels.forEach(panel=>panel.addEventListener('click',toggleOpen));
